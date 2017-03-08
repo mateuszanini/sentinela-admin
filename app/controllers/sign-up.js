6954d4698b0fea7922215aa4a31631c77a38d4d5
Tiago Possato <tiago.possato@yahoo.com.br>
2017-03-08 00:24:53 -0300

    Resolvido problema no create user

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,21 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    firebase: Ember.inject.service(),
+    firebaseApp: Ember.inject.service(),
     actions: {
         signUp() {
-            let controller = this;
-            this.get('firebase').createUser({
-                email: this.get('email') || '',
-                password: this.get('password') || '',
-            }, (error, data) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    controller.set('email', null);
-                    controller.set('password', null);
-                    console.log(data);
-                }
+            // let controller = this;
+            const auth = this.get('firebaseApp').auth();
+            auth.createUserWithEmailAndPassword(this.get('email'), this.get('password')).then((userResponse) => {
+                alert('deu');
+                // const user = this.store.createRecord('user', {
+                //     id: userResponse.uid,
+                //     email: userResponse.email
+                // });
+                // return user.save();
             });
         }
     }
